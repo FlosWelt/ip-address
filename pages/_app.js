b7fd7874c5d466dfe8b4e0ba9887093b7145dc8e
FlosWelt <76884340+FlosWelt@users.noreply.github.com>
2022-03-11 18:51:04 +0100

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,25 @@
 import '../styles/globals.css'
 import NextNProgress from "nextjs-progressbar";
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return <>
+        <Head>
+        <title>Search Ip Address</title>
+        <meta name="description" content="The best place to get your Ip Address or lookup Ip Addresses without Ads" />
+        <link rel="icon" href="/logo.png" />
+        <meta name="viewport"></meta>
+        <meta property="og:type" content="website"></meta>
+        <meta property="og:url" content="https://ip-address-omega.vercel.app/"></meta>
+        <meta property="og:title" content="Ip Address - The place for IP Addresses"></meta>
+        <meta property="og:description" content="The best place to get your Ip Address or lookup Ip Addresses without Ads"></meta>
+        <meta property="og:image" content="/og-img.png"></meta>
+        <meta property="twitter:card" content="summary_large_image"></meta>
+        <meta property="twitter:url" content="https://ip-address-omega.vercel.app/"></meta>
+        <meta property="twitter:title" content="Ip Address - The place for IP Addresses"></meta>
+        <meta property="twitter:description" content="The best place to get your Ip Address or lookup Ip Addresses without Ads"></meta>
+        <meta property="twitter:image" content="/og-img.png"></meta>
+      </Head>
   <NextNProgress eight={6} color="#4338C9" />
   <Component {...pageProps} />
   
