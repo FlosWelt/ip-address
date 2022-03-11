08402e94b1833019f9507d1e9d1229fd7367507c
FlosWelt <76884340+FlosWelt@users.noreply.github.com>
2022-03-11 17:44:15 +0100

    Add files via upload

--- pages/privacy.js
+++ pages/privacy.js
@@ -20,7 +20,7 @@ export default function Home() {
         <h1 className="text-white font-semibold text-center text-xl my-4">Privacy</h1>
         <p className="text-center text-white">Ip Address doesnt store your Ip in any way it just displays it. </p>
         <p className="text-center text-white">We use <a className="underline" href="https://ipify.org" target="_blank" rel="noreferrer" >ipify.org</a> to get your Ip Address, your country is fetched from <a className="underline" href="https://country.is" target="_blank" rel="noreferrer" >country.is</a>.</p>
-        <p className="text-center text-white">The Ip Lookup function is powerd by <a className="underline" href="https://www.ip-api.com" target="_blank" rel="noreferrer" >ip-api.com</a></p>
+        <p className="text-center text-white">The Ip Lookup function is powerd by <a className="underline" href="https://ipwhois.io" target="_blank" rel="noreferrer" >ipwhois.io</a></p>
         <p className="text-center text-white">We also use <a className="underline" href="https://www.cloudflare.com/web-analytics/" target="_blank" rel="noreferrer" >Cloudflare Webanalytics</a> as a privacy friedly analytics software</p>
         <p className="text-center text-white ">By further conserns you can mail me under <a className="underline" href="mailto:contact@floswelt.com" target="_blank" rel="noreferrer" >contact@floswelt.com</a></p>
         <p className="sm:my-80 my-40"></p>
