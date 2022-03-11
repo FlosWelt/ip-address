0548c6b2c86d88e42c6dccb7aff06666877e8143
FlosWelt <76884340+FlosWelt@users.noreply.github.com>
2022-03-11 22:47:48 +0100

    Add files via upload

--- components/footer.js
+++ components/footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
 
     return (
         <div>
-        <div className="bg-gray-900 text-white font-semibold border-t-2">
+        <div className="bg-gray-900 text-white font-semibold border-t-2 sticky bottom-0 2xl:fixed ">
             <h1 className="p-2 inline-block"> © {new Date().getFullYear()} Ip Address</h1>
             <Link href="mailto:contact@floswelt.com" >
             <a className="inline-block text-gray-300 hover:text-slate-500 px-2" >contact@floswelt.com</a>
